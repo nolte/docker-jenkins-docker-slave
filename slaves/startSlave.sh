8d28aa13afb0eadc292fca88a4868956137d3b52
nolte <nolte@ingo.de>
2016-03-19 21:46:45 +0100

    using parameter for the JENKINS_MASTER_HOST and JENKINS_MASTER_PORT

--- slaves/startSlave.sh
+++ slaves/startSlave.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 SLAVE_JAR="/tmp/slave.jar"
 
-java -jar $SLAVE_JAR -jnlpUrl http://jenkinshost:8080/jenkins/computer/$JENKINS_SLAVE_NODE/slave-agent.jnlp
+java -jar $SLAVE_JAR -jnlpUrl http://$JENKINS_MASTER_HOST:$JENKINS_MASTER_PORT/jenkins/computer/$JENKINS_SLAVE_NODE/slave-agent.jnlp
